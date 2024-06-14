a4d7a38785705ea217c00357b5785d65c1fb3c08
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-06-14 15:18:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -706,4 +706,4 @@ def get_feedback_for_answer(answer_id):
         return "Bad Request", 404
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=8000, debug=True)
+    app.run(host='127.0.0.1',port=8001, debug=True)
