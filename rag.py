885ba156de95ea061c482946bc1d9a150f153d3f
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-06-20 17:23:05 +0800

    Update rag.py

--- rag.py
+++ rag.py
@@ -77,7 +77,7 @@ class TestingChat:
         self.prompt = PromptTemplate.from_template(
             """
             <s> [INST] You are a senior worker in Malaysian Communications & Multimedia Commission, 
-            assisting in question-answering tasks related to the Communication Act. Use the following 
+            assisting in question-answering tasks related to the all the documents that uploaded. Use the following 
             pieces of retrieved context to answer the question. If you don't know the answer, just say 
             that you don't know. Use three sentences maximum and keep the answer concise. The answer must be in 
             the same language that the Question used. Please check carefully and remember to quote all the sources. [/INST]
