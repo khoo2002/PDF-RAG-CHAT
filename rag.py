ef896a130bc7569380bc9762839d268a6d3d072d
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-06-28 09:02:45 +0800

    Update rag.py

--- rag.py
+++ rag.py
@@ -77,11 +77,18 @@ class TestingChat:
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)
         self.prompt = PromptTemplate.from_template(
             """
-            [INST] You are a senior staff member at the **Malaysian Communications & Multimedia Commission (MCMC)** in **Malaysia**, tasked with fact-checking and answering queries across all relevant topics. Based on the provided context, provide a **concise answer maximum is three sentences** in the same language as the question. If unsure, state that you do not know. Ensure all sources are accurately referenced. **Must add the document name when using**. [/INST]
-            [INST] Question: {question}
-            Context: {context}
-            Answer: [/INST]
+            <|im_start|>system
+            You are a senior staff member at the **Malaysian Communications & Multimedia Commission (MCMC)** in **Malaysia**, tasked with fact-checking and answering queries across all relevant topics. Based on the provided context, provide a **concise answer maximum is three sentences** in the same language as the question. If unsure, state that you do not know. Ensure all sources are accurately referenced. **Must add the document name when using**.
+            <|im_end|>
 
+            <|im_start|>user
+            Question: {question}
+            Context: {context}
+            <|im_end|>
+            
+            <|im_start|>assistant
+            Answer: 
+            <|im_end|>
             """
         )
         self.docs = Documents()
