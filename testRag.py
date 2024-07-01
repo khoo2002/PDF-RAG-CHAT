df28498cef054c60a89c0de5638cbd8a19c4d6d6
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-01 09:01:14 +0800

    Update testRag.py

--- testRag.py
+++ testRag.py
@@ -2,5 +2,10 @@ from langchain_community.vectorstores import Milvus
 from langchain_community.embeddings import OllamaEmbeddings
 
 embedding_model = OllamaEmbeddings(model='nomic-embed-text')
-vector_store = Milvus(embedding_function=embedding_model, , collection_name = 'LangChainCollection', drop_old = False)
+mil = Milvus(embedding_function=embedding_model, , collection_name = 'LangChainCollection', drop_old = False)
+vector_store = mil.as_retriever(
+    search_type="mmr",
+    search_kwargs={'k': 2, 'lambda_mult': 0.8}
+)
+
 print(vector_store)
