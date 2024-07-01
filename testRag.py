d99922c0a422f17976c26c5a56bc41592c1c18d4
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-01 08:54:14 +0800

    Update testRag.py

--- testRag.py
+++ testRag.py
@@ -2,5 +2,5 @@ from langchain_community.vectorstores import Milvus
 from langchain_community.embeddings import OllamaEmbeddings
 
 embedding_model = OllamaEmbeddings(model='nomic-embed-text')
-vector_store = Milvus(embedding=embedding_model)
+vector_store = Milvus(embedding_function=embedding_model, , collection_name = 'LangChainCollection', drop_old = False)
 print(vector_store)
