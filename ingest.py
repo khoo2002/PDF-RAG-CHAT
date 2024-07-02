4b34281c790e05a4b2e628bf2eda0c4ce9b73d61
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-02 12:16:13 +0800

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -4,8 +4,47 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.utils import filter_complex_metadata
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 import sys
+import duckdb
+import os
+
+PARENT_DATABASE = '../database/' 
+DATABASE_PATH = '../database/testing.db'
+if os.path.exists(PARENT_DATABASE) != True:
+    os.mkdir(PARENT_DATABASE)
+
+def list_pdf_filenames(path):
+    """
+    Prints the names of all PDF files in the given directory path.
+    """
+    # Check if the given path is a directory
+    if not os.path.isdir(path):
+        print("The provided path is not a valid directory.")
+        return
+
+    # List all files in the directory
+    files = os.listdir(path)
+    
+    # Filter and print PDF files
+    pdf_files = [file for file in files if file.endswith('.pdf')]
+    if pdf_files:
+        print("PDF files in the directory:")
+        return pdf_files
+    else:
+        print("No PDF files found in the directory.")
 
 def ingest_from_path(paths):
+    conn = duckdb.connect(DATABASE_PATH)
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS pdf_files (
+            id INTEGER PRIMARY KEY,
+            file_path TEXT,
+            file_name TEXT
+        );
+        
+        CREATE SEQUENCE IF NOT EXISTS seq_fileid START 1;
+    """.format(file_path = os.path.join(path,filename), file_name = filename))
+    conn.close()
+
     for path in paths:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)
         embedding_model = OllamaEmbeddings(model='nomic-embed-text')
@@ -17,6 +56,15 @@ def ingest_from_path(paths):
         print(f"Number of chunks: {len(chunks)}")
         mil.aadd_documents(chunks)
         print("Ingest done for path:", path)
+        pdf_files = list_pdf_filenames(path)
+        for file in pdf_files:
+            conn = duckdb.connect(DATABASE_PATH)
+            conn.execute("""
+            INSERT INTO pdf_files 
+            VALUES (nextval('seq_fileid'),'{file_path}', '{file_name}')
+            ON CONFLICT (id) DO NOTHING;
+            """.format(file_path = os.path.join(path,filename), file_name = filename))
+            conn.close()
     return True
 
 if __name__ == '__main__':
