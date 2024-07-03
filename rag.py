259881b36d68e6ecb3a0cf47af89778566d12a99
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-03 17:09:16 +0800

    Update rag.py

--- rag.py
+++ rag.py
@@ -20,9 +20,9 @@ UPLOAD_FOLDER = '../uploaded'
 class TestingChat:
 
     def __init__(self):
-        self.qwen2Model = ChatOllama(model="qwen2:0.5b")
-        self.gemmaModel = ChatOllama(model="gemma:2b")
-        self.phi3Model = ChatOllama(model="phi3:latest", top_k=10, top_p=0.5)
+        self.qwen2Model = ChatOllama(model="qwen2:0.5b", temperature=0.4)
+        self.gemmaModel = ChatOllama(model="gemma:2b", temperature=0.4)
+        self.phi3Model = ChatOllama(model="phi3:latest", top_k=10, top_p=0.5, temperature=0.4)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)
         self.qwen2Prompt = PromptTemplate.from_template(
             """
