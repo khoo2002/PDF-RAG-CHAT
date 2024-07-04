a988c6b505d56fac328c77da1344b0ddd2732e43
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-04 08:57:00 +0800

    Update rag.py

--- rag.py
+++ rag.py
@@ -20,10 +20,10 @@ UPLOAD_FOLDER = '../uploaded'
 class TestingChat:
 
     def __init__(self):
-        self.qwen2Model = ChatOllama(model="qwen2:0.5b-instruct-q4_0", temperature=0.4)
-        self.gemmaModel = ChatOllama(model="gemma:2b-instruct-q4_0", temperature=0.4)
-        self.phi3Model = ChatOllama(model="phi3:3.8b-instruct", top_k=10, top_p=0.5, temperature=0.4, num_predict=80)
-        # self.llama3Model = ChatOllama(model="llama3:latest", top_k=10, top_p=0.5, temperature=0.4, num_predict=80)
+        # self.qwen2Model = ChatOllama(model="qwen2:0.5b-instruct-q4_0", temperature=0.4)
+        # self.gemmaModel = ChatOllama(model="gemma:2b-instruct-q4_0", temperature=0.4)
+        # self.phi3Model = ChatOllama(model="phi3:3.8b-instruct", top_k=10, top_p=0.5, temperature=0.4, num_predict=80)
+        self.llama3Model = ChatOllama(model="llama3:latest", top_k=10, top_p=0.5, temperature=0.4, num_predict=80)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)
         self.qwen2Prompt = PromptTemplate.from_template(
             """
@@ -62,6 +62,18 @@ class TestingChat:
         <|senior staff|>
         **Answer**:
         [Choose the better option based on the context. If needed, restructure the original answers based on the given context. If necessary, provide a new answer based on the question and context]
+        **Document Name**: [Insert Document Name]
+        """
+        self.llama3Prompt = """
+
+        <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+        You are a senior staff member at the **Malaysian Communications & Multimedia Commission (MCMC)** in **Malaysia**, tasked with fact-checking and answering queries across all relevant topics. Based on the provided context, provide a **concise answer maximum is three sentences** in the same language as the question. If unsure, state that you do not know. Ensure all sources are accurately referenced. **Must add the document name when using**.
+        **Context**: {context} <|eot_id|>
+        <|start_header_id|>user<|end_header_id|>
+        **Question**: {question} <|eot_id|>
+        <|start_header_id|>senior staff<|end_header_id|>
+        **Answer**:
+        
         **Document Name**: [Insert Document Name]
         """
         
@@ -123,12 +135,18 @@ class TestingChat:
         
     
     def ask(self, query: str):
-        qwen2Option = self.qwen2Chain.invoke(query)
-        gemmaOption = self.gemmaChain.invoke(query)
-        self.phi3Prompt = self.phi3Prompt.format(question='{question}', option1=qwen2Option, option2=gemmaOption, context='{context}')
-        self.phi3Prompt = PromptTemplate.from_template(self.phi3Prompt)
-        self.phi3Chain = ({"context": self.retriever, "question": RunnablePassthrough()}
-          | self.phi3Prompt
-          | self.phi3Model
-          | StrOutputParser())        
-        return self.phi3Chain.invoke(query)
+        # qwen2Option = self.qwen2Chain.invoke(query)
+        # gemmaOption = self.gemmaChain.invoke(query)
+        # self.phi3Prompt = self.phi3Prompt.format(question='{question}', option1=qwen2Option, option2=gemmaOption, context='{context}')
+        # self.phi3Prompt = PromptTemplate.from_template(self.phi3Prompt)
+        # self.phi3Chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+        #   | self.phi3Prompt
+        #   | self.phi3Model
+        #   | StrOutputParser()) 
+        self.llama3Chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                  | self.llama3Prompt
+                  | self.llama3Model
+                  | StrOutputParser())
+        
+        # return self.phi3Chain.invoke(query)
+        return self.llama3Chain.invoke(query)
