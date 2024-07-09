dc1dfbb2d7954f2442f85e83f1e0d5c5b5008e49
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-09 16:24:47 +0800

    Update rag.py

--- rag.py
+++ rag.py
@@ -13,6 +13,7 @@ from langchain.vectorstores.utils import filter_complex_metadata
 import os
 import sys
 from PyPDF2 import PdfMerger
+import re
 # from documents import Documents
 adding_pack = """Section 1, Short title
 Section 2, Commencement
@@ -436,29 +437,80 @@ class TestingChat:
         
     
     def ask(self, query: str):
-        '''
-        qwen2Option = self.qwen2Chain.invoke(query)
-        gemmaOption = self.gemmaChain.invoke(query)
-        self.phi3Prompt = self.phi3Prompt.format(question='{question}', option1=qwen2Option, option2=gemmaOption, context='{context}')
-        self.phi3Prompt = PromptTemplate.from_template(self.phi3Prompt)
-        self.phi3Chain = ({"context": self.retriever, "question": RunnablePassthrough()}
-          | self.phi3Prompt
-          | self.phi3Model
-          | StrOutputParser()) 
-        '''
-        # return self.phi3Chain.invoke(query)
+        self.llama3Model = ChatOllama(model="llama3:latest", top_k=10, top_p=0.5, temperature=0.4)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)  
+        self.llama3Prompt = None
+        if re.search("Section", text) or re.search("CMA", text) or re.search("COMMUNICATIONS AND MULTIMEDIA ACT", text):
+            self.llama3Prompt = PromptTemplate.from_template("""
+            <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+            You are a senior staff member at the **Malaysian Communications & Multimedia Commission (MCMC)** in **Malaysia**, tasked with fact-checking and answering queries across all relevant topics. Based on the provided context, provide a **concise answer maximum is three sentences** in the same language as the question. If unsure, state that you do not know. Ensure all sources are accurately referenced. **Must add the document name when using**.
+            **Context**: {context} {adding_pack}. The current Prime Minister is Dato' Sri Anwar Ibrahim from 24 November 2022 until now.<|eot_id|>
+            <|start_header_id|>user<|end_header_id|>
+            **Question**: {question} <|eot_id|>
+            <|start_header_id|>senior staff<|end_header_id|>
+            **Answer**:
+            
+            **Document Name**: [Insert Document Name]
+            """.format(context="{context}",adding_pack=adding_pack,question="{question}"))
+        else:
+            self.llama3Prompt = PromptTemplate.from_template("""
+        <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+        You are a senior staff member at the **Malaysian Communications & Multimedia Commission (MCMC)** in **Malaysia**, tasked with fact-checking and answering queries across all relevant topics. Based on the provided context, provide a **concise answer maximum is three sentences** in the same language as the question. If unsure, state that you do not know. Ensure all sources are accurately referenced. **Must add the document name when using**.
+        **Context**: {context}. The current Prime Minister is Dato' Sri Anwar Ibrahim from 24 November 2022 until now.<|eot_id|>
+        <|start_header_id|>user<|end_header_id|>
+        **Question**: {question} <|eot_id|>
+        <|start_header_id|>senior staff<|end_header_id|>
+        **Answer**:
+        
+        **Document Name**: [Insert Document Name]
+        """)    
+        
+        embedding_model = OllamaEmbeddings(model='nomic-embed-text')
+        mil = Milvus(embedding_function=embedding_model, collection_name = 'LangChainCollection', drop_old = False)
+        self.vector_store = mil
+        self.retriever = self.vector_store.as_retriever()
+        self.llama3Chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                            | self.llama3Prompt
+                            | self.llama3Model
+                            | StrOutputParser())
         return self.llama3Chain.invoke(query)
 
+
     def askByStream(self, query: str):
-        '''
-        qwen2Option = self.qwen2Chain.invoke(query)
-        gemmaOption = self.gemmaChain.invoke(query)
-        self.phi3Prompt = self.phi3Prompt.format(question='{question}', option1=qwen2Option, option2=gemmaOption, context='{context}')
-        self.phi3Prompt = PromptTemplate.from_template(self.phi3Prompt)
-        self.phi3Chain = ({"context": self.retriever, "question": RunnablePassthrough()}
-          | self.phi3Prompt
-          | self.phi3Model
-          | StrOutputParser()) 
-        '''
-        # return self.phi3Chain.invoke(query)
+        self.llama3Model = ChatOllama(model="llama3:latest", top_k=10, top_p=0.5, temperature=0.4)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)  
+        self.llama3Prompt = None
+        if re.search("Section", text) or re.search("CMA", text) or re.search("COMMUNICATIONS AND MULTIMEDIA ACT", text):
+            self.llama3Prompt = PromptTemplate.from_template("""
+            <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+            You are a senior staff member at the **Malaysian Communications & Multimedia Commission (MCMC)** in **Malaysia**, tasked with fact-checking and answering queries across all relevant topics. Based on the provided context, provide a **concise answer maximum is three sentences** in the same language as the question. If unsure, state that you do not know. Ensure all sources are accurately referenced. **Must add the document name when using**.
+            **Context**: {context} {adding_pack}. The current Prime Minister is Dato' Sri Anwar Ibrahim from 24 November 2022 until now.<|eot_id|>
+            <|start_header_id|>user<|end_header_id|>
+            **Question**: {question} <|eot_id|>
+            <|start_header_id|>senior staff<|end_header_id|>
+            **Answer**:
+            
+            **Document Name**: [Insert Document Name]
+            """.format(context="{context}",adding_pack=adding_pack,question="{question}"))
+        else:
+            self.llama3Prompt = PromptTemplate.from_template("""
+        <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+        You are a senior staff member at the **Malaysian Communications & Multimedia Commission (MCMC)** in **Malaysia**, tasked with fact-checking and answering queries across all relevant topics. Based on the provided context, provide a **concise answer maximum is three sentences** in the same language as the question. If unsure, state that you do not know. Ensure all sources are accurately referenced. **Must add the document name when using**.
+        **Context**: {context}. The current Prime Minister is Dato' Sri Anwar Ibrahim from 24 November 2022 until now.<|eot_id|>
+        <|start_header_id|>user<|end_header_id|>
+        **Question**: {question} <|eot_id|>
+        <|start_header_id|>senior staff<|end_header_id|>
+        **Answer**:
+        
+        **Document Name**: [Insert Document Name]
+        """)    
+        
+        embedding_model = OllamaEmbeddings(model='nomic-embed-text')
+        mil = Milvus(embedding_function=embedding_model, collection_name = 'LangChainCollection', drop_old = False)
+        self.vector_store = mil
+        self.retriever = self.vector_store.as_retriever()
+        self.llama3Chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                            | self.llama3Prompt
+                            | self.llama3Model
+                            | StrOutputParser())
         return self.llama3Chain.stream(query)
