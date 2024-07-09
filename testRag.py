f4a9ebfcf4de8f4c8327e5b02e00ba6d71acc671
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-09 16:45:20 +0800

    Update testRag.py

--- testRag.py
+++ testRag.py
@@ -1,5 +1,5 @@
-from langchain_community.vectorstores import Milvus
-from langchain_community.embeddings import OllamaEmbeddings
+# from langchain_community.vectorstores import Milvus
+# from langchain_community.embeddings import OllamaEmbeddings
 from rag import TestingChat
 
 import itertools
