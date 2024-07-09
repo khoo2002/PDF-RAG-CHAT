9cd48729f0da40911b0e5816ec27070b54e4ddd5
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-09 09:37:25 +0800

    Update testRag.py

--- testRag.py
+++ testRag.py
@@ -5,11 +5,9 @@ from rag import TestingChat
 embedding_model = OllamaEmbeddings(model='nomic-embed-text')
 mil = Milvus(embedding_function=embedding_model, collection_name = 'LangChainCollection', drop_old = False)
 vector_store = mil.as_retriever(
-    search_type="mmr",
-    search_kwargs={'k': 2, 'lambda_mult': 0.8}
 )
 
-mil.max_marginal_relevance_search("What should the terminal equipment be capable of in terms of data message format?")
+mil.max_marginal_relevance_search("What is section 233 in CMA?")
 print(vector_store)
 
 
