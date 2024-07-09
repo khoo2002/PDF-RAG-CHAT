89b4e8f38f1fe16cc233247479c45c1c191ed98f
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-09 16:40:33 +0800

    Update testRag.py

--- testRag.py
+++ testRag.py
@@ -2,17 +2,31 @@ from langchain_community.vectorstores import Milvus
 from langchain_community.embeddings import OllamaEmbeddings
 from rag import TestingChat
 
-embedding_model = OllamaEmbeddings(model='nomic-embed-text')
-mil = Milvus(embedding_function=embedding_model, collection_name = 'LangChainCollection', drop_old = False)
-vector_store = mil.as_retriever(
-)
+import itertools
+import time
+from flask import Flask, Response, redirect, request, url_for
+import json
 
-mil.similarity_search("What is section 233 in CMA?")
-print(vector_store)
+app = Flask(__name__)
 
+
+# embedding_model = OllamaEmbeddings(model='nomic-embed-text')
+# mil = Milvus(embedding_function=embedding_model, collection_name = 'LangChainCollection', drop_old = False)
+# vector_store = mil.as_retriever(
+# )
+#embedding_query = embedding_model.embed_query("What is section 233 in CMA?")
+#print(mil.similarity_search("What is section 233 in CMA?", k=10))
 
 test = TestingChat()
+        
+@app.route('/streamByJson')
+def stream_json():
+    # Stream JSON data
+    def generate(query):
+        for i in enumerate(test.askByStream(query)):
+            json_data = json.dumps({"response": i})
+            yield f"data: {json_data}\n\n"
+    return Response(generate(), mimetype='text/event-stream')
 
-for tet in test.askByStream("What is CMA?"):
-    print(tet)
-    print(type(tet))
+if __name__ == "__main__":
+    app.run(host='localhost', port=23423)
