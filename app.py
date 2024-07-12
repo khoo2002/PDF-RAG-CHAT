e7d37d01c0d6b6caec4da8be573ae48eac801a85
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-12 15:47:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -458,7 +458,18 @@ app.config.update(
     CELERY_BROKER_URL='redis://localhost:6379/0',
     CELERY_RESULT_BACKEND='redis://localhost:6379/0'
 )
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+    return celery
+
 celery = make_celery(app)
+#celery done
 
 if os.path.exists(UPLOAD_FOLDER) != True:
     os.mkdir(UPLOAD_FOLDER)
