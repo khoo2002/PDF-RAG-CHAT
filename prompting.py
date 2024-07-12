6ca3784ae7316443dd6cdb998f9d7e4b0834ba6b
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-12 08:49:06 +0000

    test

---
+++ prompting.py
@@ -0,0 +1,31 @@
+import duckdb
+from rag import TestingChat
+from app import Answer
+import datetime
+from threading import Thread
+import sys
+
+
+def generate(question, question_id):
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    test = TestingChat()
+    response_text = test.ask(question)
+    print(response_text)
+    # Store the answer
+    answer_record = {
+        'question_id': self.question_id,  # Use the same question ID as the question
+        'answer_text': response_text,
+        'created_at': timestamp
+    }
+    answer = Answer.store_answer(answer_record)
+    answer = Answer.get_answer(question_id)
+    answer_id = answer['answer'][0]['answer_id']
+    print('done')
+
+    return response_text
+
+if __name__ == "__main__":
+    thread = Thread(target = generate, args = (sys.argv[3],sys.argv[4] ))
+    thread.start()
+    thread.join()
+    print("thread finished...exiting")
