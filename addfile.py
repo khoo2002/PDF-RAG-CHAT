21732ef713fd73f807bcf8f3fc00a398d1f20b70
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2024-07-19 15:41:53 +0800

    Update addfile.py

--- addfile.py
+++ addfile.py
@@ -1,8 +1,13 @@
+import duckdb
+import os
+
+files = os.listdir('../uploaded/')
+
 for file in pdf_files:
   conn = duckdb.connect(DATABASE_PATH)
   conn.execute("""
   INSERT INTO pdf_files 
   VALUES (nextval('seq_fileid'),'{file_path}', '{file_name}')
   ON CONFLICT (id) DO NOTHING;
-  """.format(file_path = os.path.join(path,file), file_name = file))
+  """.format(file_path = os.path.join('../uploaded/',file), file_name = file))
   conn.close()
